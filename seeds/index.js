4e7876d5df12c5f26894cf4d0510a67056913a95
Jack <li.jack0707@gmail.com>
2023-04-28 09:50:23 -0400

    chore: add unsplash random collection image to seed data & price & description

--- seeds/index.js
+++ seeds/index.js
@@ -19,10 +19,13 @@ const seedDB = async() => {
     
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 30) + 10
         const camp = new Campground({
         title: `${sample(descriptors)} ${sample(places)}`,
         location: `${cities[random1000].city}, ${cities[random1000].state}`,
-    
+        image:`https://source.unsplash.com/collection/483251`,
+        description: " Lorem Ipsum ",
+        price
         });
         await camp.save()
     }
