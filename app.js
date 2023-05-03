69c1929f73f92b24757dbe1f7790f7935cee6e6f
Jack <li.jack0707@gmail.com>
2023-05-03 16:17:24 -0400

    feat: add Joi schema validation to middleware before submitting to database

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const ExpressError = require('./utils/ExpressError')
 const catchAsync = require('./utils/catchAsync')
+const Joi = require('joi')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -43,7 +44,25 @@ app.get("/campgrounds/new", (req, res) => {
 
 // save the campground to the database
 app.post('/campgrounds', catchAsync(async(req, res, next)=>{
-    if(!req.body.campground) throw new ExpressError("Invalid Campground Data", 400)
+    // if(!req.body.campground) throw new ExpressError("Invalid Campground Data", 400)
+    const campgroundSchema = Joi.object({ 
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required(),
+
+        }).required()
+    })
+    const { error} = campgroundSchema.validate(req.body)
+    console.log(error)
+
+    if(error){
+        const msg = error.details.map( el => el.message).join('')
+        throw new ExpressError(msg, 400)
+    }
+
     const campground = new Campground(req.body.campground);
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
