14aecd96be0be4da6ebff19a47b217c73054f49c
Jack <li.jack0707@gmail.com>
2023-05-03 16:33:28 -0400

    refactor: moved joi schema to a different file

--- app.js
+++ app.js
@@ -6,26 +6,37 @@ const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const ExpressError = require('./utils/ExpressError')
 const catchAsync = require('./utils/catchAsync')
-const Joi = require('joi')
+const { CampgroundSchema } = require("./schemas.js")
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
 const db = mongoose.connection;
+const app = express();
 
 db.on("error", console.error.bind(console, "connection error:"))
 db.once("open", ()=>{
     console.log("database connected")
 })
-
-
-const app = express();
-
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, "views"))
 
 app.engine('ejs', ejsMate)
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
+
+
+const validateCampground = (req, res, next) => {
+    
+    const { error } = CampgroundSchema.validate(req.body);
+    if (error) {
+    const msg = error.details.map((el) => el.message).join("");
+    throw new ExpressError(msg, 400);
+    }
+    else{
+    next()
+    }
+}
+
 
 app.get('/', (req,res)=>{
     res.render('home')
@@ -43,26 +54,9 @@ app.get("/campgrounds/new", (req, res) => {
 });
 
 // save the campground to the database
-app.post('/campgrounds', catchAsync(async(req, res, next)=>{
+app.post('/campgrounds', validateCampground, catchAsync(async(req, res, next)=>{
     // if(!req.body.campground) throw new ExpressError("Invalid Campground Data", 400)
-    const campgroundSchema = Joi.object({ 
-        campground: Joi.object({
-            title: Joi.string().required(),
-            price: Joi.number().required().min(0),
-            image: Joi.string().required(),
-            location: Joi.string().required(),
-            description: Joi.string().required(),
-
-        }).required()
-    })
-    const { error} = campgroundSchema.validate(req.body)
-    console.log(error)
-
-    if(error){
-        const msg = error.details.map( el => el.message).join('')
-        throw new ExpressError(msg, 400)
-    }
-
+  
     const campground = new Campground(req.body.campground);
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
@@ -83,11 +77,17 @@ app.get('/campgrounds/:id/edit', catchAsync(async(req,res)=>{
 }))
 
 // save edited data to db
-app.put('/campgrounds/:id', catchAsync(async(req,res)=>{
-    const {id} = req.params
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}, {new: true});
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
+app.put(
+  "/campgrounds/:id", validateCampground, catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(
+      id,
+      { ...req.body.campground },
+      { new: true }
+    );
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
 
 
 app.delete('/campgrounds/:id', catchAsync(async (req,res)=>{
