fc75a806a5c68320fa39d6d116e03ca770bd1858
Jack <li.jack0707@gmail.com>
2023-05-09 10:31:27 -0400

    feat: add delete button and delete route

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.delete('/campgrounds/:id', catchAsync(async (req,res)=>{
 
 
 // reviews
-app.post('/campgrounds/:id/review', validateReview, catchAsync(async (req,res)=>{
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req,res)=>{
     const campground = await Campground.findById(req.params.id)
     const review = new Review(req.body.review)
     campground.reviews.push(review)
@@ -115,6 +115,14 @@ app.post('/campgrounds/:id/review', validateReview, catchAsync(async (req,res)=>
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`);
 }))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync( async (req,res)=>{
+    const {id, reviewId}= req.params
+    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
+    await Review.findByIdAndDelete(reviewId)
+    res.redirect(`/campgrounds/${id}`)
+}))
+
 
 app.all('*', (req, res, next)=>{
     next(new ExpressError("Page Not Found", 404))
