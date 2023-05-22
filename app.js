9e45c83dcf428bdb718f31a606a2905da66e0580
Jack <li.jack0707@gmail.com>
2023-05-22 09:28:38 -0400

    chore: add customized Joi validation to prevent cross-site scripting

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const flash = require('connect-flash')
 const passport = require('passport')
 const LocalStrategy= require('passport-local')
 const User = require('./models/user')
+const mongoSanitize = require('express-mongo-sanitize')
 
 const campgroundRoutes = require("./routes/campgrounds")
 const reviewRoutes = require("./routes/reviews")
@@ -36,6 +37,7 @@ app.engine('ejs', ejsMate)
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize())
 
 const sessionConfig = {
   secret: "sessionsecret",
